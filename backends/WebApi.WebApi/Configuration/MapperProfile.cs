5eaf6165214803aa5b8ab0cac4fc1524496258b1
bad fad <badfad@edu.novia.fi>
2023-08-12 20:42:13 +0300

    configured MapperProfile

--- backends/WebApi.WebApi/Configuration/MapperProfile.cs
+++ backends/WebApi.WebApi/Configuration/MapperProfile.cs
@@ -12,6 +12,42 @@ namespace WebApi.WebApi.Configuration
     {
         public MapperProfile()
         {
+            CreateMap<CartItem,CartItemReadDto>();
+            CreateMap<CartItemUpdateDto, CartItem>();
+            CreateMap<CartItemCreateDto, CartItem>();
+
+            CreateMap<Category,CategoryReadDto>();
+            CreateMap<CategoryUpdateDto, Category>();
+            CreateMap<CategoryCreateDto, Category>();
+
+            CreateMap<OrderDetail,OrderDetailReadDto>();
+            CreateMap<OrderDetailUpdateDto, OrderDetail>();
+            CreateMap<OrderDetailCreateDto, OrderDetail>();
+
+            CreateMap<Order,OrderReadDto>();
+            CreateMap<OrderUpdateDto, Order>();
+            CreateMap<OrderCreateDto, Order>();
+
+            CreateMap<Payment,PaymentReadDto>();
+            CreateMap<PaymentUpdateDto, Payment>();
+            CreateMap<PaymentCreateDto, Payment>();
+
+            CreateMap<Product,ProductReadDto>();
+            CreateMap<ProductUpdateDto, Product>();
+            CreateMap<ProductCreateDto, Product>();
+
+            CreateMap<Review,ReviewReadDto>();
+            CreateMap<ReviewUpdateDto, Review>();
+            CreateMap<ReviewCreateDto, Review>();
+
+            CreateMap<Shipping,ShippingReadDto>();
+            CreateMap<ShippingUpdateDto, Shipping>();
+            CreateMap<ShippingCreateDto, Shipping>();
+
+            CreateMap<ShoppingCart,ShoppingCartReadDto>();
+            CreateMap<ShoppingCartUpdateDto, ShoppingCart>();
+            CreateMap<ShoppingCartCreateDto, ShoppingCart>();
+
             CreateMap<User,UserReadDto>();
             CreateMap<UserUpdateDto, User>();
             CreateMap<UserCreateDto, User>();
